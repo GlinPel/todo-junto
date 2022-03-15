7ac14e28c6b177159c0df12e814674398adc5a9c
GleenPeltroche <gleenpeltroche@gmail.com>
2022-03-14 22:06:32 -0500

    apoyalo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';    
+import PersonCard from './PersonCard';
+    
+class App extends Component {
+    render() {
+        return <div>
+          <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+          <PersonCard firstName={"Jhon"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+          <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+          <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
+        </div>;
+    }
 }
-
+    
 export default App;
